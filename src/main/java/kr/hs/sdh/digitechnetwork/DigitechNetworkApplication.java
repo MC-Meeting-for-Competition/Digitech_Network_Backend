2230fa04d44b03a68adbc78dadf69a585e9ca579
balhyo-younjisang <yjs12180825@gmail.com>
2025-08-29 10:33:51 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/kr/hs/sdh/digitechnetwork/DigitechNetworkApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.sdh.digitechnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitechNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitechNetworkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/hs/sdh/digitechnetwork/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  Digitech Network Application 의 Controller 클래스들을 모은 패키지입니다.
+ *
+ * @since 2025.08.29
+ * @version 0.0.1
+ * @author 윤지상 - sdh230308@sdh.hs.kr
+ */
+package kr.hs.sdh.digitechnetwork.controller;
