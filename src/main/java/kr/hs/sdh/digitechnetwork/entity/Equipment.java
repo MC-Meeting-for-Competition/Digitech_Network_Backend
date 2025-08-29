c553f03b91d873b1ab8c0c056701b6220c441bd2
balhyo-younjisang <yjs12180825@gmail.com>
2025-08-29 13:23:06 +0900

    feat(entity): Equipment Entity, EquipmentType Entity 추가
    Entity 를 위해 BaseEntity 추가 ( Created, Updated 필드 )
    Equipment Entity 와 EquipmentType Entity 추가

---
+++ src/main/java/kr/hs/sdh/digitechnetwork/entity/Equipment.java
@@ -0,0 +1,65 @@
+package kr.hs.sdh.digitechnetwork.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+enum EquipmentStatus {
+    AVAILABLE("대여 가능한 기자재"),
+    RENT("대여 중인 기자재"),
+    BROKEN("고장난 기자재"),
+    CHECK("반납 처리 중인 기자재"),
+    UNAVAILABLE("사용이 불가능한 기자재"),
+    FIX("수리 중인 기자재");
+
+    private final String message;
+
+    EquipmentStatus(String message) {
+        this.message = message;
+    }
+}
+
+@Entity
+@Table(name = "equipments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Equipment extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "equipments_id")
+    private Long id;
+
+    @Column(unique = true)
+    private String identifier;
+
+    @Column(nullable = false)
+    private Boolean isPublic;
+
+    @Enumerated(EnumType.STRING)
+    private EquipmentStatus status;
+
+    @Column(nullable = true)
+    private String cautionMessage;
+
+    Equipment setStatus(EquipmentStatus status) {
+        this.status = status;
+        return this;
+    }
+
+    Equipment setPublic(Boolean isPublic) {
+        this.isPublic = isPublic;
+        return this;
+    }
+
+    Equipment setIdentifier(String identifier) {
+        this.identifier = identifier;
+        return this;
+    }
+
+    Equipment setCautionMessage(String cautionMessage) {
+        this.cautionMessage = cautionMessage;
+        return this;
+    }
+}
---
+++ src/main/java/kr/hs/sdh/digitechnetwork/entity/EquipmentType.java
@@ -0,0 +1,45 @@
+package kr.hs.sdh.digitechnetwork.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "equipment_types")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EquipmentType extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String type;
+
+    @Column(nullable = true)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean isPublic;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "equipments_id")
+    Collection<Equipment> equipmentList;
+
+    public EquipmentType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("[" + id + "]" + type);
+        for (Equipment equipment : equipmentList) {
+            result.append("\n").append(equipment.toString());
+        }
+
+        return result.toString();
+    }
+}
