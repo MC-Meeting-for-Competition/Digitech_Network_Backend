1755be70bf892c88a12edbc867a4962390083780
balhyo-younjisang <yjs12180825@gmail.com>
2025-08-30 21:54:40 +0900

    refactor: equipmentStatus 분리

--- src/main/java/kr/hs/sdh/digitechnetwork/entity/Equipment.java
+++ src/main/java/kr/hs/sdh/digitechnetwork/entity/Equipment.java
@@ -1,30 +1,8 @@
 package kr.hs.sdh.digitechnetwork.entity;
 
 import jakarta.persistence.*;
+import kr.hs.sdh.digitechnetwork.enums.EquipmentStatus;
 import lombok.*;
-
-/**
- * 기자재의 상태를 ENUM 만들어 데이터베이스에 저장
- *
- * @since 2025.08.29
- * @author yunjisang - sdh230308@sdh.hs.kr
- * @version 0.0.1
- */
-@Getter
-enum EquipmentStatus {
-    AVAILABLE("대여 가능한 기자재"),
-    RENT("대여 중인 기자재"),
-    BROKEN("고장난 기자재"),
-    CHECK("반납 처리 중인 기자재"),
-    UNAVAILABLE("사용이 불가능한 기자재"),
-    FIX("수리 중인 기자재");
-
-    private final String message;
-
-    EquipmentStatus(String message) {
-        this.message = message;
-    }
-}
 
 /**
  * Equipment 엔티티
